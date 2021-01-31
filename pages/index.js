ded13d93a1cae11fe26a4c06a53e146ddc50bd56
Paulo Reis <prsc.reis@gmail.com>
2021-01-31 14:33:34 -0300

    Commit inicial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,116 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
-  return <Title>My page</Title>
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          {db.title}
+        </title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={(infosDoEvento) => {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+                const projectURL = githubUser ? `/quiz/${projectName}___${githubUser}` : `/quiz/${projectName}`;
+                const projectLabel = githubUser ? `${githubUser}/${projectName}` : projectName;
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={name.length === 0 ? 'label' : Link}
+                      href={projectURL}
+                      data-disabled={name.length === 0}
+                    >
+                      {projectLabel}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
+          </Widget.Content>
+        </Widget>
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/prscreis" />
+    </QuizBackground>
+  );
 }
